ca08804dc0a877516047aaccfd626e8568a34726
Deepak Choudhari <deepak.choudhari@siemens-healthineers.com>
2019-07-14 14:42:11 -0500

    Added more integration tests for MoviesController

--- WheelMovies.IntegrationTests/WheelMovies_IntegrationTests.cs
+++ WheelMovies.IntegrationTests/WheelMovies_IntegrationTests.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using WheelMovies.Business.DTO;
 using Xunit;
 
 namespace WheelMovies.IntegrationTests
@@ -25,6 +31,38 @@ namespace WheelMovies.IntegrationTests
 
             // 3. Assert
             response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task GetMoviesForUser_Returns_NotFound_For_No_UserId()
+        {
+            // 1. Arrange
+            var client = factory.CreateClient();
+
+            // 2. Act
+            var response = await client.GetAsync("/api/movies/user");
+
+            // 3. Assert
+            Assert.True(response.StatusCode == HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task GetMoviesByCriteriaRequest_For_ValidRequest_Returns_MoviesList()
+        {
+            // 1. Arrange
+            var client = factory.CreateClient();
+            var request = new GetMoviesByCriteriaRequest
+            {
+                RunningTime = 120
+            };
+            var json = JsonConvert.SerializeObject(request);
+            var payload = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // 2. Act
+            var response = await client.PostAsync("/api/movies", payload);
+
+            // 3. Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
---
+++ WheelMovies.Tests/MoviesControllerTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace WheelMovies.Tests
+{
+    public class MoviesControllerTests
+    {
+        //TODO: Add unit tests for MoviesController
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
